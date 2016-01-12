9ef7a94112ce027f8f8bc350242999e94dad043e
Jaden Geller <jaden.geller@gmail.com>
2016-01-11 17:03:28 -0800

    Initial Commit

---
+++ Erratic/Erratic.h
@@ -0,0 +1,19 @@
+//
+//  Erratic.h
+//  Erratic
+//
+//  Created by Jaden Geller on 1/11/16.
+//  Copyright © 2016 Jaden Geller. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Erratic.
+FOUNDATION_EXPORT double ErraticVersionNumber;
+
+//! Project version string for Erratic.
+FOUNDATION_EXPORT const unsigned char ErraticVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Erratic/PublicHeader.h>
+
+
